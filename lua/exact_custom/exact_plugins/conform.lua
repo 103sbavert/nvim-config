076aca46451fa1ad28f2371be51d994965aed123
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-06-25 02:25:01 +0530

    feat: set prettier flags based on filetype

--- lua/exact_custom/exact_plugins/conform.lua
+++ lua/exact_custom/exact_plugins/conform.lua
@@ -1,48 +1,48 @@
-require('conform').setup {
+require("conform").setup({
     formatters_by_ft = {
-        lua = { 'stylua' },
-        python = { 'isort', 'black' },
-        go = { 'gofmt' },
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', stop_after_first = true },
-        json = { 'prettierd', 'prettier', stop_after_first = true },
-        yaml = { 'prettierd', 'prettier', stop_after_first = true },
-        toml = { 'taplo' },
-        sh = { 'shfmt' },
-        bash = { 'shfmt' },
-        zsh = { 'shfmt' },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        go = { "gofmt" },
+        javascript = { "prettier", stop_after_first = true },
+        typescript = { "prettier", stop_after_first = true },
+        json = { "prettier", stop_after_first = true },
+        yaml = { "prettier", stop_after_first = true },
+        toml = { "taplo" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+        zsh = { "shfmt" },
     },
     format_on_save = {
         timeout_ms = 500,
-        lsp_format = 'fallback',
+        lsp_format = "fallback",
     },
     formatters = {
         stylua = {
-            args = { '--indent-type', 'Spaces', '--indent-width', '4', '-' },
+            args = { "--indent-type", "Spaces", "--indent-width", "4", "-" },
         },
         shfmt = {
-            args = { '-i', '4', '-ci' },
-        },
-        prettierd = {
-            args = { '--tab-width', '4', '$FILENAME' },
+            args = { "-i", "4", "-ci" },
         },
         prettier = {
-            args = { '--tab-width', '4', '--stdin-filepath', '$FILENAME' },
+            args = function(_, ctx)
+                local ft = vim.bo[ctx.buf].filetype
+                return { "--parser", ft, "--log-level", "error", "--tab-width", "4", "--stdin-filepath", "$FILENAME" }
+            end,
         },
         taplo = {
-            args = { 'fmt', '--option', 'indent_string=    ', '-' },
+            args = { "fmt", "--option", "indent_string=    ", "-" },
         },
     },
-}
+})
 
-vim.api.nvim_create_user_command('Conformat', function(args)
+vim.api.nvim_create_user_command("Conformat", function(args)
     local range = nil
     if args.count ~= -1 then
         local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
         range = {
             start = { args.line1, 0 },
-            ['end'] = { args.line2, end_line:len() },
+            ["end"] = { args.line2, end_line:len() },
         }
     end
-    require('conform').format { astnc = true, lsp_format = 'fallback', range = range }
-end, { range = true, desc = 'Format current buffer or visual selection with conform' })
+    require("conform").format({ astnc = true, lsp_format = "fallback", range = range })
+end, { range = true, desc = "Format current buffer or visual selection with conform" })
