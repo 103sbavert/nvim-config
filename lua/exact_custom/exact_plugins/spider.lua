a0aa925cc921fa0411d8a47b7aafb5a9c5b6c5ad
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-06-29 14:54:19 +0530

    refactor: change camelCase toggle key to <leader>cc from <leader>tc

--- lua/exact_custom/exact_plugins/spider.lua
+++ lua/exact_custom/exact_plugins/spider.lua
@@ -1,25 +1,31 @@
 local function toggle_buffer_camel_mode()
-  -- enable per buffer
-  local bufnr = vim.api.nvim_get_current_buf()
-  local is_active = vim.b[bufnr].camel_mode_active or false
-  local modes = { "n", "o", "x" }
+    -- enable per buffer
+    local bufnr = vim.api.nvim_get_current_buf()
+    local is_active = vim.b[bufnr].camel_mode_active or false
+    local modes = { "n", "o", "x" }
 
-  if not is_active then
-    vim.b[bufnr].camel_mode_active = true
-    vim.notify("Enabling camelCase mode")
-    
-    vim.keymap.set(modes, "w", function() require("spider").motion("w") end, { buffer = bufnr, desc = "Spider-w" })
-    vim.keymap.set(modes, "b", function() require("spider").motion("b") end, { buffer = bufnr, desc = "Spider-b" })
-    vim.keymap.set(modes, "e", function() require("spider").motion("e") end, { buffer = bufnr, desc = "Spider-e" })
-  else
-    vim.b[bufnr].camel_mode_active = false
-    vim.notify("Disabling camelCase mode")
-    
-    vim.keymap.del(modes, "w", { buffer = bufnr })
-    vim.keymap.del(modes, "b", { buffer = bufnr })
-    vim.keymap.del(modes, "e", { buffer = bufnr })
-  end
+    if not is_active then
+        vim.b[bufnr].camel_mode_active = true
+        vim.notify("Enabling camelCase mode")
+
+        vim.keymap.set(modes, "w", function()
+            require("spider").motion("w")
+        end, { buffer = bufnr, desc = "Spider-w" })
+        vim.keymap.set(modes, "b", function()
+            require("spider").motion("b")
+        end, { buffer = bufnr, desc = "Spider-b" })
+        vim.keymap.set(modes, "e", function()
+            require("spider").motion("e")
+        end, { buffer = bufnr, desc = "Spider-e" })
+    else
+        vim.b[bufnr].camel_mode_active = false
+        vim.notify("Disabling camelCase mode")
+
+        vim.keymap.del(modes, "w", { buffer = bufnr })
+        vim.keymap.del(modes, "b", { buffer = bufnr })
+        vim.keymap.del(modes, "e", { buffer = bufnr })
+    end
 end
 
 -- toggle on demand
-vim.keymap.set("n", "<leader>tc", toggle_buffer_camel_mode, { desc = "Toggle Buffer CamelCase Mode" })
+vim.keymap.set("n", "<leader>cc", toggle_buffer_camel_mode, { desc = "Toggle Buffer CamelCase Mode" })
