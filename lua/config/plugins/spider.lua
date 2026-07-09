c6c2448eb8d548c9bb8f56714cebeb973d3942f0
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-07-09 23:14:13 +0530

    refactor: use map_toggle_key for keybinding and fix description

--- lua/config/plugins/spider.lua
+++ lua/config/plugins/spider.lua
@@ -4,23 +4,23 @@ local function toggle_buffer_camel_mode()
     local bufnr = vim.api.nvim_get_current_buf()
     local is_active = vim.b[bufnr].camel_mode_active or false
     local modes = { "n", "o", "x" }
+    local state
 
     if not is_active then
         vim.b[bufnr].camel_mode_active = true
-        vim.notify("Enabling camelCase mode")
-
+        state = "on"
         vim.keymap.set(modes, "w", function() require("spider").motion("w") end, { buffer = bufnr, desc = "Spider-w" })
         vim.keymap.set(modes, "b", function() require("spider").motion("b") end, { buffer = bufnr, desc = "Spider-b" })
         vim.keymap.set(modes, "e", function() require("spider").motion("e") end, { buffer = bufnr, desc = "Spider-e" })
     else
         vim.b[bufnr].camel_mode_active = false
-        vim.notify("Disabling camelCase mode")
-
+        state = "off"
         vim.keymap.del(modes, "w", { buffer = bufnr })
         vim.keymap.del(modes, "b", { buffer = bufnr })
         vim.keymap.del(modes, "e", { buffer = bufnr })
     end
+
+    return true, "camelCase navigation turned " .. state
 end
 
--- toggle on demand
-vim.keymap.set("n", "<leader>tc", toggle_buffer_camel_mode, { desc = "Toggle Buffer CamelCase Mode" })
+map_toggle_key("c", toggle_buffer_camel_mode, "Buffer [c]amelCase Mode")
