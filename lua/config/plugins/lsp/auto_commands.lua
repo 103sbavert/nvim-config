600e50a69123feafa40ea4a5174608611735e113
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-07-12 16:57:57 +0530

    refactor: create new auto_commands.lua for lsp auto commands

---
+++ lua/config/plugins/lsp/auto_commands.lua
@@ -0,0 +1,42 @@
+local group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = group,
+    callback = function(event)
+        -- highlight references of symbol under cursor, unhighlight when cursor leaves the symbol
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+        if client and client:supports_method("textDocument/documentHighlight", event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd("LspDetach", {
+                group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+                callback = function(event2)
+                    vim.lsp.buf.clear_references()
+                    vim.api.nvim_clear_autocmds({ group = highlight_augroup, buffer = event2.buf })
+                end,
+            })
+        end
+
+        local function toggle_hints()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+            return nil, false
+        end
+
+        if client and client:supports_method("textDocument/inlayHint", event.buf) then
+            map_toggle_key("h", toggle_hints, "Inlay [h]ints")
+        end
+    end,
+})
