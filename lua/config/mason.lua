dbbe86db30b0fa44374225f9e3fd6c92d728a46b
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-07-15 20:23:27 +0530

    feat: add mason.lua for cumulative mason package installation

---
+++ lua/config/mason.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+require("mason").setup({})
+local mason_tool_installer = require("mason-tool-installer")
+
+local ensure_installed = {}
+local debounce_hrs = 6
+
+local InstallTools = function(tool_list)
+    ensure_installed = vim.tbl_deep_extend("force", ensure_installed, tool_list)
+
+    mason_tool_installer.setup({
+        ensure_installed = ensure_installed,
+        run_on_start = false,
+    })
+end
+
+local group = vim.api.nvim_create_augroup("mason-install-tools", {
+    clear = true,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = group,
+    callback = function() mason_tool_installer.check_install(true, false) end,
+})
+
+M.InstallTools = InstallTools
+
+return M
