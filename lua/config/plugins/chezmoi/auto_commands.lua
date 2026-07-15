9f8e81d3bbd45950b9084bbab5f1901b1ebeeefd
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-07-16 01:19:09 +0530

    feat: add robust validation against invalid files, faster shortcuits

--- lua/config/plugins/chezmoi/auto_commands.lua
+++ lua/config/plugins/chezmoi/auto_commands.lua
@@ -18,61 +18,17 @@ local watched_src_files = {}
 vim.api.nvim_create_autocmd("BufReadPre", {
     group = open_src_grp,
     callback = function(args)
+        if no_open_src_files then
+            return
+        end
+
         local buf_id = args.buf
+        if not vim.api.nvim_buf_is_valid(buf_id) then
+            return
+        end
 
-        vim.schedule(function()
-            if not vim.api.nvim_buf_is_valid(buf_id) or vim.bo[buf_id].buftype ~= "" then
-                return
-            end
-
-            local buf_file = vim.api.nvim_buf_get_name(buf_id)
-            if buf_file == "" or no_open_src_files then
-                return
-            end
-
-            utils.is_src_file(buf_file, function(is_src)
-                if is_src then
-                    return
-                end
-
-                utils.should_ignore_src_file(buf_file, function(should_ignore)
-                    if should_ignore then
-                        return
-                    end
-
-                    utils.get_src_file(buf_file, function(sources)
-                        if not sources or #sources == 0 then
-                            return
-                        end
-
-                        local source = sources[1]
-                        if not source or not vim.uv.fs_stat(source) or source == buf_file then
-                            return
-                        end
-
-                        if utils.has_symlink_attr(source) then
-                            return
-                        end
-
-                        utils.ask_open_src_file(function(choice)
-                            if choice == 2 then
-                                cmd_edit:exec(buf_file)
-                            elseif choice == 3 then
-                                no_open_src_files = true
-                            end
-                        end)
-                    end)
-                end)
-            end)
-        end)
-    end,
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-    group = apply_src_grp,
-    callback = function(args)
-        local buf_id = args.buf
-        if vim.bo[buf_id].buftype ~= "" then
+        local buf_type = vim.bo[buf_id].buftype
+        if not buf_type or buf_type == "" then
             return
         end
 
@@ -81,48 +37,111 @@ vim.api.nvim_create_autocmd("BufWritePost", {
             return
         end
 
-        utils.should_ignore_src_file(buf_file, function(should_ignore)
-            if should_ignore then
+        utils.is_src_file(buf_file, function(is_src)
+            if is_src then
                 return
             end
 
-            utils.is_src_file(buf_file, function(is_src)
-                if not is_src then
+            utils.should_ignore_src_file(buf_file, function(should_ignore)
+                if should_ignore then
                     return
                 end
 
-                if no_apply_src_files[buf_file] then
-                    return
-                elseif watched_src_files[buf_file] then
-                    cmd_apply:async({ buf_file })
-                    return
-                end
-
-                utils.ask_apply_src_file(function(choice)
-                    if choice == 2 or choice == 4 then
-                        cmd_apply:async({ buf_file, "--source-path" }, function(result)
-                            if result.success then
-                                vim.notify("Applied to target", vim.log.levels.INFO, { title = "Chezmoi" })
-                            end
-                        end)
+                utils.get_src_file(buf_file, function(sources)
+                    if not sources or #sources == 0 then
+                        return
                     end
 
-                    if choice == 3 then
-                        no_apply_src_files[buf_file] = true
-                    elseif choice == 4 then
-                        watched_src_files[buf_file] = true
-                        vim.notify("File will be auto-applied on save", vim.log.levels.INFO, { title = "Chezmoi" })
+                    local source = sources[1]
+                    if not source or not vim.uv.fs_stat(source) or source == buf_file then
+                        return
                     end
+
+                    if utils.has_symlink_attr(source) then
+                        return
+                    end
+
+                    utils.ask_open_src_file(function(choice)
+                        if choice == 2 then
+                            cmd_edit:exec(buf_file)
+                        elseif choice == 3 then
+                            no_open_src_files = true
+                        end
+                    end)
                 end)
             end)
         end)
     end,
 })
 
+utils.get_src_dir(function(src_dir)
+    local src_dir_pattern = { src_dir .. "/*.tmpl", src_dir .. "/**/*.tmpl" }
+
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        group = apply_src_grp,
+        pattern = src_dir_pattern,
+        callback = function(args)
+            local buf_id = args.buf
+            if not vim.api.nvim_buf_is_valid(buf_id) then
+                return
+            end
+
+            local buf_type = vim.bo[buf_id].buftype
+            if not buf_type or buf_type == "" then
+                return
+            end
+
+            local buf_file = vim.api.nvim_buf_get_name(buf_id)
+            if buf_file == "" then
+                return
+            end
+
+            if no_apply_src_files[buf_file] then
+                return
+            end
+
+            utils.should_ignore_src_file(buf_file, function(should_ignore)
+                if should_ignore then
+                    return
+                end
+
+                utils.is_src_file(buf_file, function(is_src)
+                    if not is_src then
+                        return
+                    end
+
+                    if watched_src_files[buf_file] then
+                        cmd_apply:async({ buf_file })
+                        return
+                    end
+
+                    utils.ask_apply_src_file(function(choice)
+                        if choice == 2 or choice == 4 then
+                            cmd_apply:async({ buf_file, "--source-path" }, function(result)
+                                if result.success then
+                                    vim.notify("Applied to target", vim.log.levels.INFO, { title = "Chezmoi" })
+                                end
+                            end)
+                        end
+
+                        if choice == 3 then
+                            no_apply_src_files[buf_file] = true
+                        elseif choice == 4 then
+                            watched_src_files[buf_file] = true
+                            vim.notify("File will be auto-applied on save", vim.log.levels.INFO, { title = "Chezmoi" })
+                        end
+                    end)
+                end)
+            end)
+        end,
+    })
+end)
+
 vim.api.nvim_create_autocmd({ "BufDelete", "BufFilePre" }, {
     group = apply_src_grp,
     callback = function(args)
         local buf_file = vim.api.nvim_buf_get_name(args.buf)
+
         no_apply_src_files[buf_file] = nil
         watched_src_files[buf_file] = nil
     end,
