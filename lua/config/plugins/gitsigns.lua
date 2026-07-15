0a4740ed6c2af1760d33eb9e3fb919c2dcbdeb59
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-07-15 20:26:01 +0530

    fix: update description and keymaps for gitsigns to match behavior

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -4,72 +4,95 @@ return {
     config = function()
         require("gitsigns").setup({
             signs = {
-                add = { text = "+" }, ---@diagnostic disable-line: missing-fields
-                change = { text = "~" }, ---@diagnostic disable-line: missing-fields
-                delete = { text = "_" }, ---@diagnostic disable-line: missing-fields
-                topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
-                changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+                untracked = { text = "┆" },
             },
+            attach_to_untracked = true,
             on_attach = function(bufnr)
                 local gitsigns = require("gitsigns")
 
+                -- Initialize mappers
                 local git_key_mapper = create_keymap_group("[g]it", "<leader>g", { "n", "v" })
+                local git_reset_mapper = create_keymap_group("[r]eset", "<leader>gr", { "n", "v" })
                 local navigate_bw_mapper = create_keymap_group("[ backwards", "[", { "n", "v" })
                 local navigate_fw_mapper = create_keymap_group("] forwards", "]", { "n", "v" })
 
                 -- Navigation
-                navigate_fw_mapper("c", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "]c", bang = true })
-                    else
-                        gitsigns.nav_hunk("next")
-                    end
-                end, "Jump to next git [c]hange")
+                do
+                    navigate_fw_mapper("c", function()
+                        if vim.wo.diff then
+                            return "]c"
+                        end
+                        vim.schedule(function() gitsigns.nav_hunk("next") end)
+                        return "<Ignore>"
+                    end, "Jump to next git [c]hange")
 
-                navigate_bw_mapper("c", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "[c", bang = true })
-                    else
-                        gitsigns.nav_hunk("prev")
-                    end
-                end, "Jump to previous git [c]hange")
+                    navigate_bw_mapper("c", function()
+                        if vim.wo.diff then
+                            return "[c"
+                        end
+                        vim.schedule(function() gitsigns.nav_hunk("prev") end)
+                        return "<Ignore>"
+                    end, "Jump to previous git [c]hange")
+                end
 
-                -- hunk staging - visual mode
-                git_key_mapper(
-                    "h",
-                    function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
-                    "Stage [h]unk",
-                    nil,
-                    "v"
-                )
+                -- Staging
+                do
+                    git_key_mapper(
+                        "h",
+                        function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+                        "Toggle [h]unk staging",
+                        nil,
+                        { "v" }
+                    )
+                    git_key_mapper("h", gitsigns.stage_hunk, "Toggle [h]unk staging", nil, { "n" })
+                    git_key_mapper("s", gitsigns.stage_buffer, "[s]tage buffer")
+                    git_key_mapper("u", gitsigns.reset_buffer_index, "[u]nstage buffer")
+                end
 
-                git_key_mapper(
-                    "H",
-                    function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
-                    "Unstage [H]unk",
-                    nil,
-                    "v"
-                )
+                -- History
+                do
+                    git_key_mapper("b", function() gitsigns.blame_line({ full = true }) end, "[b]lame line")
+                    git_key_mapper("d", gitsigns.diffthis, "View [d]iff against index")
+                    git_key_mapper("D", function() gitsigns.diffthis("@") end, "View [D]iff against HEAD")
+                end
 
-                -- hunk staging - normal mode
-                git_key_mapper("h", gitsigns.stage_hunk, "Stage [h]unk")
-                git_key_mapper("H", gitsigns.stage_hunk, "Unstage [H]unk")
+                -- Resets
+                do
+                    git_reset_mapper("h", gitsigns.reset_hunk, "Reset [h]unk changes", nil, { "n" })
+                    git_reset_mapper("b", gitsigns.reset_buffer, "Reset [b]uffer changes")
+                    git_reset_mapper(
+                        "h",
+                        function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+                        "Reset [h]unk changes",
+                        nil,
+                        { "v" }
+                    )
+                end
 
-                -- buffer staging - normal mode
-                git_key_mapper("s", gitsigns.stage_buffer, "[s]tage buffer", nil, "n")
-                git_key_mapper("u", gitsigns.reset_buffer, "[u]nstage buffer", nil, "n")
+                -- Text object
+                do
+                    vim.keymap.set(
+                        { "o", "x" },
+                        "ih",
+                        ":<C-U>Gitsigns select_hunk<CR>",
+                        { buffer = bufnr, desc = "Select hunk" }
+                    )
+                end
 
-                -- history - diff and blame
-                git_key_mapper("b", function() gitsigns.blame_line({ full = true }) end, "[b]lame line")
-                map_toggle_key("b", gitsigns.toggle_current_line_blame, "Current line [b]lame")
-                git_key_mapper("d", gitsigns.diffthis, "View [d]iff against index")
-                git_key_mapper("D", function() gitsigns.diffthis("@") end, "View [D]iff against index")
+                -- LazyGit
+                do
+                    git_key_mapper("g", "<cmd>LazyGitCurrentFile<cr>", "Initialize LazyGit TUI")
+                end
 
-                -- text object 'h'unk
-                vim.keymap.set({ "o", "x" }, "ih", gitsigns.select_hunk, { buffer = bufnr })
-
-                -- LazyGit TUI with kdheepak/lazygit.nvim
-                git_key_mapper("g", "<cmd>LazyGitCurrentFile<cr>", "Initialize LazyGit TUI")
+                -- Toggles
+                do
+                    map_toggle_key("b", gitsigns.toggle_current_line_blame, "Current line [b]lame")
+                end
             end,
         })
     end,
