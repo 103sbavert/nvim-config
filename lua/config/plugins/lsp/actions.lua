1e169b6d5b5a9b3f6badbc4fdf7f15b5ccb8b83b
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-07-17 14:51:18 +0530

    feat: prevent eager loading of LSP modules with lazy_require

--- lua/config/plugins/lsp/actions.lua
+++ lua/config/plugins/lsp/actions.lua
@@ -1,63 +1,12 @@
-local osh_wrapper = require("omnisharp_extended")
-local ts_builtin = require("telescope.builtin")
+local UT = require("config.utils")
 local utils = require("config.plugins.lsp.utils")
+local get_telescope_builtin = UT.lazy_require("telescope.builtin")
+local get_omnisharp_ext = UT.lazy_require("omnisharp_extended")
 
 --- @class LspJumpConfig
 --- @field default_lsp_action function Callback function executed for standard language servers.
 --- @field lsp_action_override? table<string, function> Optional map of LSP client names to override callbacks.
 --- @field description string Documentation string for the keymap decoration.
-
---- @type table<string, LspJumpConfig>
-local lsp_jump = {
-    ["n"] = {
-        description = "Re[n]ame Symbol",
-        default_lsp_action = vim.lsp.buf.rename,
-    },
-    ["a"] = {
-        description = "[a]ction",
-        default_lsp_action = vim.lsp.buf.code_action,
-    },
-    ["D"] = {
-        description = "[D]eclaration",
-        default_lsp_action = vim.lsp.buf.declaration,
-    },
-    ["sW"] = {
-        description = "[s]ymbols in [W]orkspace",
-        default_lsp_action = ts_builtin.lsp_document_symbols,
-    },
-    ["sD"] = {
-        description = "[s]ymbols in [D]ocument",
-        default_lsp_action = ts_builtin.lsp_dynamic_workspace_symbols,
-    },
-    ["t"] = {
-        default_lsp_action = ts_builtin.lsp_type_definitions,
-        lsp_action_override = {
-            ["omnisharp"] = osh_wrapper.telescope_lsp_type_definition,
-        },
-        description = "[t]ype definition",
-    },
-    ["r"] = {
-        default_lsp_action = ts_builtin.lsp_references,
-        lsp_action_override = {
-            ["omnisharp"] = osh_wrapper.telescope_lsp_references,
-        },
-        description = "[r]eferences",
-    },
-    ["i"] = {
-        default_lsp_action = ts_builtin.lsp_implementations,
-        lsp_action_override = {
-            ["omnisharp"] = osh_wrapper.telescope_lsp_implementation,
-        },
-        description = "[i]mplementation",
-    },
-    ["d"] = {
-        default_lsp_action = ts_builtin.lsp_definitions,
-        lsp_action_override = {
-            ["omnisharp"] = osh_wrapper.telescope_lsp_definitions,
-        },
-        description = "[d]efinition",
-    },
-}
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("telescope_lsp_action", { clear = true }),
@@ -68,6 +17,58 @@ vim.api.nvim_create_autocmd("LspAttach", {
         end
 
         local client_name = client.name
+
+        --- @type table<string, LspJumpConfig>
+        local lsp_jump = {
+            ["n"] = {
+                description = "Re[n]ame Symbol",
+                default_lsp_action = vim.lsp.buf.rename,
+            },
+            ["a"] = {
+                description = "[a]ction",
+                default_lsp_action = vim.lsp.buf.code_action,
+            },
+            ["D"] = {
+                description = "[D]eclaration",
+                default_lsp_action = vim.lsp.buf.declaration,
+            },
+            ["sW"] = {
+                description = "[s]ymbols in [W]orkspace",
+                default_lsp_action = function() get_telescope_builtin().lsp_document_symbols() end,
+            },
+            ["sD"] = {
+                description = "[s]ymbols in [D]ocument",
+                default_lsp_action = function() get_telescope_builtin().lsp_dynamic_workspace_symbols() end,
+            },
+            ["t"] = {
+                default_lsp_action = function() get_telescope_builtin().lsp_type_definitions() end,
+                lsp_action_override = {
+                    ["omnisharp"] = function() get_omnisharp_ext().telescope_lsp_type_definition() end,
+                },
+                description = "[t]ype definition",
+            },
+            ["r"] = {
+                default_lsp_action = function() get_telescope_builtin().lsp_references() end,
+                lsp_action_override = {
+                    ["omnisharp"] = function() get_omnisharp_ext().telescope_lsp_references() end,
+                },
+                description = "[r]eferences",
+            },
+            ["i"] = {
+                default_lsp_action = function() get_telescope_builtin().lsp_implementations() end,
+                lsp_action_override = {
+                    ["omnisharp"] = function() get_omnisharp_ext().telescope_lsp_implementation() end,
+                },
+                description = "[i]mplementation",
+            },
+            ["d"] = {
+                default_lsp_action = function() get_telescope_builtin().lsp_definitions() end,
+                lsp_action_override = {
+                    ["omnisharp"] = function() get_omnisharp_ext().telescope_lsp_definitions() end,
+                },
+                description = "[d]efinition",
+            },
+        }
 
         for key, config in pairs(lsp_jump) do
             local target_fn = config.default_lsp_action
