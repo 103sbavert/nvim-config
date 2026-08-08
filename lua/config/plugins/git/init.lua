efc048786c9ecf3c8dbb8013651bd93f344ba129
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-08 23:32:58 +0530

    fix(git): disable nvr in LazyGit plugin

--- lua/config/plugins/git/init.lua
+++ lua/config/plugins/git/init.lua
@@ -95,6 +95,7 @@ return {
             do
                 vim.g.lazygit_floating_window_scaling_factor = 0.80
                 vim.g.lazygit_floating_window_use_plenary = 1
+                vim.g.lazygit_use_neovim_remote = 0
 
                 git_key_mapper("l", utils.open_lazy_git, "Open [l]azyGit")
             end
