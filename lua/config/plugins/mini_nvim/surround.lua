239b589e92e6db912af9ebafe41a2409a23f01da
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-09 22:54:33 +0530

    feat: accept textobject after s for surround add

--- lua/config/plugins/mini_nvim/surround.lua
+++ lua/config/plugins/mini_nvim/surround.lua
@@ -1,6 +1,11 @@
--- Add/delete/replace surroundings (brackets, quotes, etc.)
---
--- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
--- - sd'   - [S]urround [D]elete [']quotes
--- - sr)'  - [S]urround [R]eplace [)] [']
-require("mini.surround").setup()
+require("mini.surround").setup({
+    mappings = {
+        add = "s", -- Add surrounding around textobject (e.g., siw), saw", s$)
+        delete = "sd", -- Delete surrounding (e.g., sd", sd()
+        find = "sf", -- Find next surrounding to the right (e.g., sf")
+        find_left = "sF", -- Find previous surrounding to the left (e.g., sF")
+        highlight = "sh", -- Highlight surrounding (e.g., sh")
+        replace = "sr", -- Replace surrounding pair (e.g., sr"')
+        update_n_lines = "sn", -- Adjust max search line range for detecting surroundings
+    },
+})
