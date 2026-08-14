88261e767398ad31eef78960f5e4d238ef9f8010
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-15 01:16:35 +0530

    fix(terminal): preserve and toggle terminal buffer visibility

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -1,17 +1,28 @@
 local term_buf = nil
+local term_win = nil
 
 local function toggle_terminal()
+    -- Hide terminal if window is open and valid
+    if term_win and vim.api.nvim_win_is_valid(term_win) then
+        vim.api.nvim_win_close(term_win, false)
+        term_win = nil
+        return
+    end
+
+    -- Re-open window if valid buffer exists
     if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
-        vim.api.nvim_buf_delete(term_buf, { force = true })
-        term_buf = nil
-    else
-        vim.cmd("split | term")
-        term_buf = vim.api.nvim_get_current_buf()
+        term_win = vim.api.nvim_open_win(term_buf, true, { split = "below" })
+        return
+    end
+
+    -- Create new buffer and terminal job if no valid buffer exists
+    term_buf = vim.api.nvim_create_buf(false, false)
+    term_win = vim.api.nvim_open_win(term_buf, true, { split = "below" })
+
+    local chan_id = vim.fn.jobstart({ vim.o.shell }, { term = true })
+    if chan_id <= 0 then
+        vim.notify("Unable to open the terminal", vim.log.levels.ERROR)
     end
 end
 
 map_toggle_key("t", toggle_terminal, "Bottom Terminal Pane")
-
-vim.keymap.set("t", "<C-n>", "<C-\\><C-n>", {
-    desc = "Exit terminal mode to normal mode",
-})
