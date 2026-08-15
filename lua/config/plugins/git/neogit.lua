9f8f94f5b0d991f803b855bd14773c585175cc01
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-16 05:28:50 +0530

    refactor(git): rework git integration
    - move gitsigns, LazyGit configuration into dedicated lua files
    - lazy load LazyGit on user command or key binding invokcation
    - add NeoGit integration
    - move EDITOR/GIT_EDITOR configuration to init.lua

---
+++ lua/config/plugins/git/neogit.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+return {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+        "sindrets/diffview.nvim",
+        "m00qek/baleia.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    },
+}
