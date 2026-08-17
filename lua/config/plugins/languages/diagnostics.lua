a0862263394439a0022736825424c7a55d617d0a
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-17 22:09:56 +0530

    fix(diagnostics): show diagnostic count for non-cursor lines

--- lua/config/plugins/languages/diagnostics.lua
+++ lua/config/plugins/languages/diagnostics.lua
@@ -4,12 +4,27 @@ return {
     event = "LspAttach",
     dependencies = "neovim/nvim-lspconfig",
     opts = {
+        preset = "modern",
         options = {
+            show_source = {
+                enabled = true,
+                if_many = true,
+            },
+            show_code = true,
+            use_icons_from_diagnostic = true,
+            throttle = 500,
+            add_messages = {
+                display_count = true,
+            },
             multilines = {
                 enabled = true,
-                always_show = true,
+                always_show = false,
+                trim_whitespaces = true,
             },
-            show_source = true,
+            show_related = {
+                enabled = true,
+                max_count = 3,
+            },
         },
     },
 }
