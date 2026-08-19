f2622dede5caeb2f83efe49fa95aa76401049195
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-19 08:49:59 +0530

    feat(explorer): replace neotree with Snacks explorer picker

---
+++ lua/config/plugins/snacks_nvim/init.lua
@@ -0,0 +1,42 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    main = "snacks",
+    ---@type snacks.Config
+    keys = {
+        {
+            "\\",
+            function()
+                local explorer = Snacks.picker.get({ source = "explorer" })[1]
+
+                if not explorer then
+                    Snacks.explorer()
+                elseif explorer:is_focused() then
+                    vim.cmd.wincmd("p")
+                else
+                    explorer:focus()
+                end
+            end,
+            desc = "[\\] Toggle Explorer Focus",
+        },
+    },
+    opts = {
+        bigfile = { enabled = true },
+        explorer = {
+            enabled = true,
+            replace_netrw = true,
+            trash = true,
+        },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = { enabled = true },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        statuscolumn = { enabled = true },
+        scroll = { enabled = true },
+        words = { enabled = true },
+    },
+    config = function(plugin, opts) require(plugin.main).setup(opts) end,
+}
