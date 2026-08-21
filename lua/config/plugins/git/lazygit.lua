65503a95195c877e49d460fd2065951d50b69833
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-21 08:06:53 +0530

    fix(lazygit): remove obsolete configuration options

--- lua/config/plugins/git/lazygit.lua
+++ lua/config/plugins/git/lazygit.lua
@@ -1,6 +1,7 @@
----@type LazySpec
+--@type LazySpec
 return {
     "103sbavert/lazygit.nvim",
+    branch = "fix/commit-editor-integration",
     lazy = true,
     cmd = {
         "LazyGit",
@@ -15,18 +16,6 @@ return {
     },
     keys = {
         { "<leader>gl", "<cmd>LazyGitCurrentFile<cr>", desc = "[l]azyGit" },
-    },
-    ---@type LazyGitConfig
-    opts = {
-        floating_window = {
-            scaling_factor = 0.9,
-            winblend = 0,
-            use_plenary = false,
-            border = "none",
-        },
-        neovim_remote = vim.fn.executable("nvr") == 1,
-        config_file_path = nil,
-        on_exit_callback = nil,
     },
     config = true,
 }
