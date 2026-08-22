388fae2477c31cdbc50f6abaa5ef2937d8086a1d
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-22 11:56:39 +0530

    feat: configure lazydev to source lazy.nvim plugin directories

--- lua/config/plugins/languages/lazydev.lua
+++ lua/config/plugins/languages/lazydev.lua
@@ -6,7 +6,20 @@ return {
     opts = {
         library = {
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-            { path = vim.fn.stdpath("data") .. "/site/pack/core/opt" },
+            {
+                path = vim.fs.joinpath(
+                    vim.fn.stdpath("data"),
+                    "site/pack/core/opt"
+                ),
+                words = { "vim.pack" },
+            },
+            {
+                path = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+            },
         },
+        enabled = function(root_dir)
+            local luarc = vim.fs.joinpath(root_dir, ".luarc.json")
+            return not vim.uv.fs_stat(luarc)
+        end,
     },
 }
