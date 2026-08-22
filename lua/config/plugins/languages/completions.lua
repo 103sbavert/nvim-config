78e9c2ed1b1dbf6b7a37aec12a8f114eeb7969f4
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-22 11:55:32 +0530

    feat(completions): add LazyDev to completion sources

--- lua/config/plugins/languages/completions.lua
+++ lua/config/plugins/languages/completions.lua
@@ -48,7 +48,15 @@ return {
             ghost_text = { enabled = false },
         },
         sources = {
-            default = { "lsp", "path", "snippets" },
+            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+            providers = {
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    -- make lazydev completions top priority (see `:h blink.cmp`)
+                    score_offset = 100,
+                },
+            },
         },
         fuzzy = { implementation = "rust" },
     },
