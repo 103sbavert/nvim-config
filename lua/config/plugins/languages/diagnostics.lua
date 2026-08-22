430a17d10feb54d72b595ead7ae5fd3aa45bf4a8
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-22 10:43:52 +0530

    refactor(diagnostics): remove duplicate diagnostic config in init.lua

--- lua/config/plugins/languages/diagnostics.lua
+++ lua/config/plugins/languages/diagnostics.lua
@@ -7,30 +7,23 @@ return {
     opts = {
         preset = "modern",
         options = {
-            show_source = {
-                enabled = true,
-                if_many = true,
-            },
-            show_code = true,
-            use_icons_from_diagnostic = true,
             throttle = 500,
             add_messages = {
                 display_count = true,
             },
-            multilines = {
-                enabled = true,
-                always_show = false,
-                trim_whitespaces = true,
-            },
-            show_related = {
-                enabled = true,
-                max_count = 3,
-            },
         },
     },
     init = function()
+        vim.keymap.set(
+            "n",
+            "<leader>q",
+            vim.diagnostic.setloclist,
+            { desc = "Open diagnostic [q]uickfix list" }
+        )
+
         vim.diagnostic.config({
             virtual_text = false,
+            virtual_lines = false,
             signs = {
                 text = require("config.utils").lsp_diagnostic_glyphs[vim.g.have_nerd_font],
             },
