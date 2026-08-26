307629d9ffd3fc7613a696d45f1187ee52c6d729
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-08-26 23:14:57 +0530

    feat: improve mini.surround config

--- lua/config/plugins/mini_nvim/surround.lua
+++ lua/config/plugins/mini_nvim/surround.lua
@@ -1,9 +1,9 @@
 require("mini.surround").setup({
     mappings = {
-        add = "s", -- Add surrounding around textobject (e.g., siw), saw", s$)
-        delete = "sd", -- Delete surrounding (e.g., sd", sd()
-        find = "sf", -- Find next surrounding to the right (e.g., sf")
-        find_left = "sF", -- Find previous surrounding to the left (e.g., sF")
+        add = "s", -- Add surrounding around textobject (e.g., siw", saW")
+        delete = "sd", -- Delete surrounding (e.g., sd")
+        find = "s/", -- Find next surrounding to the right (e.g., s/")
+        find_left = "s?", -- Find previous surrounding to the left (e.g., s?")
         highlight = "sh", -- Highlight surrounding (e.g., sh")
         replace = "sr", -- Replace surrounding pair (e.g., sr"')
         update_n_lines = "sn", -- Adjust max search line range for detecting surroundings
