f3e32bb11ffe3e760aafa7a2e1fc2a81544c77af
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-05 01:08:59 +0530

    feat(neogit): use codediff.nvim, map '<leader>gc' to neogit tab

--- lua/config/plugins/git/neogit.lua
+++ lua/config/plugins/git/neogit.lua
@@ -1,15 +1,21 @@
 --- @type LazySpec
 return {
     "NeogitOrg/neogit",
+    main = "neogit",
     lazy = true,
     dependencies = {
-        "sindrets/diffview.nvim",
+        "esmuellert/codediff.nvim",
         "m00qek/baleia.nvim",
         "folke/snacks.nvim",
     },
     cmd = "Neogit",
     keys = {
         { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+        {
+            "<leader>gc",
+            function() require("config.plugins.git.utils").open_commit_tab() end,
+            desc = "[c]ommit staged",
+        },
     },
     opts = {
         disable_insert_on_commit = false,
