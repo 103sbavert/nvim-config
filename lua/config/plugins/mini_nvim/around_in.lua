cb39bbd178dff09b8651bccda3afe5dbcf11baca
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-05 23:56:30 +0530

    perf(mini.nvim): load lua modules lazily in mini.ai config

--- lua/config/plugins/mini_nvim/around_in.lua
+++ lua/config/plugins/mini_nvim/around_in.lua
@@ -1,20 +1,22 @@
 local ai = require("mini.ai")
-local utils = require("config.plugins.mini_nvim.internals.ast-utils")
+local lazy_utils = function()
+    return require("config.plugins.mini_nvim.internals.ast-utils")
+end
 
 ai.setup({
     n_lines = 50,
     search_method = "cover_or_nearest",
     custom_textobjects = {
         -- Variable declarations / assignments (requires after/queries/<lang>/textobjects.scm)
-        d = utils.make_decl_textobj(),
+        d = lazy_utils().make_decl_textobj(),
 
         -- Function implementation bodies
-        f = utils.make_func_textobj(),
+        f = lazy_utils().make_func_textobj(),
 
         -- Function calls (tweaked to not detect dot in function name)
         F = ai.gen_spec.function_call({ name_pattern = "[%w_]" }),
 
         -- Blocks, loops, conditionals
-        s = utils.make_scope_textobj(),
+        s = lazy_utils().make_scope_textobj(),
     },
 })
