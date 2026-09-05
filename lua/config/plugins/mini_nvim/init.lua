5f3dea01cd78d6fcdd16963e6b3499fb306e86ba
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-05 23:28:18 +0530

    feat(mini.nvim): enable comments module

--- lua/config/plugins/mini_nvim/init.lua
+++ lua/config/plugins/mini_nvim/init.lua
@@ -9,6 +9,7 @@ return {
         require("config.plugins.mini_nvim.around_in")
         require("config.plugins.mini_nvim.surround")
         require("config.plugins.mini_nvim.statusline")
+        require("config.plugins.mini_nvim.comment")
         require("config.plugins.mini_nvim.icons")
     end,
 }
