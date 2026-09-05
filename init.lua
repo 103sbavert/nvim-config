ca856e79e6a62609d2f2b671c56e4a06b25617c8
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-05 23:58:02 +0530

    feat: enable autocmd in guess-indent plugin config

--- init.lua
+++ init.lua
@@ -325,6 +325,7 @@ require("lazy").setup({
         {
             "NMAC427/guess-indent.nvim",
             opts = {
+                auto_cmd = true,
                 on_tab_options = {
                     ["expandtab"] = true,
                 },
