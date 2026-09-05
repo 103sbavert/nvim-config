a66f9f2274b5edbcf1f6c3457222ba92edbc2072
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-05 21:31:32 +0530

    feat: improve which key config
    - enable preset plugins
    - consolidate group mappings
    - improve descriptions
    - use fixed size width for better eye parsing

--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -1,72 +1,50 @@
 --- @type LazySpec
 return {
-    main = "which-key",
     "folke/which-key.nvim",
     --- @type wk.Opts
     opts = {
-        delay = 250,
+        delay = 300,
         preset = "helix",
+        plugins = {
+            marks = true,
+            registers = true,
+            spelling = { enabled = true },
+            presets = {
+                operators = true,
+                motions = true,
+                text_objects = true,
+                windows = true,
+                nav = true,
+                z = true,
+                g = true,
+            },
+        },
         icons = { mappings = false },
+        win = {
+            width = math.floor(vim.o.columns * 0.35),
+            height = { max = math.floor(vim.o.lines * 0.5) },
+        },
+        keys = {
+            scroll_up = "<C-A>",
+            scroll_down = "<C-D>",
+        },
+        filter = function(mapping)
+            return mapping.desc and vim.trim(mapping.desc) ~= "" -- exclude if no descripton is found
+        end,
         spec = {
-            { "gw", desc = "Text formatting", mode = { "n", "v" } },
-            { "gq", desc = "LSP formatting", mode = { "n", "v" } },
-            { "ga", group = "[g]oto c[a]lls", mode = { "n", "v" } },
-            { "gc", group = "Comments", mode = { "n", "v" } },
-            { "<leader>s", group = "[s]earch", mode = { "n", "v" } },
-            { "<leader>l", group = "[l]SP", mode = { "n", "v" } },
-            { "<leader>z", group = "Che[z]moi", mode = { "n" } },
+            -- `g` prefix groups
+            -- { "gc", desc = "Comment", mode = "n" },
+            { "gq", desc = "Format (smart)", mode = "n" },
+            { "gw", desc = "Format (dumb)", mode = "n" },
+            { "ga", group = "[g]o [t]o c[a]lls", mode = "n" },
+
+            -- `<leader>` prefix groups
+            { "<leader>g", group = "[g]it", mode = "n" },
+            { "<leader>b", group = "[b]reakpoints", mode = "n" },
+            { "<leader>t", group = "[t]oggle", mode = "n" },
+            { "<leader>z", group = "Che[z]moi", mode = "n" },
+            { "<leader>s", group = "[s]earch", mode = "n" },
+            { "<leader>l", group = "[l]SP", mode = "n" },
         },
     },
-    config = function(plugin, opts)
-        --- @module "which-key"
-        local wk = require(plugin.main)
-        wk.setup(opts)
-
-        local registered_groups = {}
-
-        --- @param group_name string Label for the key group, shown in which-key pop-up
-        --- @param prefix_keys string Group prefix key sequence (such as "<leadder>F" for all key maps starting in "<leader>F")
-        --- @param default_modes string|string[] Default vim modes
-        --- @return fun(keys: string, func: string|function, desc: string, opts: vim.keymap.set.Opts?, modes?: string|string[]): nil
-        function _G.create_keymap_group(group_name, prefix_keys, default_modes)
-            if not registered_groups[group_name] then
-                wk.add({
-                    { prefix_keys, group = group_name, mode = default_modes },
-                })
-                registered_groups[group_name] = true
-            end
-
-            return function(keys, func, desc, keymap_opts, modes)
-                local final_opts =
-                    vim.tbl_deep_extend("force", {}, keymap_opts or {})
-                final_opts.desc = desc
-                local target_modes = modes or default_modes
-                local full_keys = prefix_keys .. keys
-                vim.keymap.set(target_modes, full_keys, func, final_opts)
-            end
-        end
-
-        local toggle_key_group =
-            create_keymap_group("[t]oggle", "<leader>t", { "n" })
-
-        --- @param keys string Key suffix
-        --- @param func fun(): (string|nil, boolean|nil) Function returning message and notify flag
-        --- @param desc string Keymap description
-        function _G.map_toggle_key(keys, func, desc)
-            local function toggle_fn()
-                local message, should_notify = func()
-                if should_notify and message and message ~= "" then
-                    vim.notify(message, vim.log.levels.INFO)
-                end
-            end
-
-            toggle_key_group(keys, toggle_fn, desc)
-            vim.keymap.set(
-                { "n" },
-                "<leader>t" .. keys,
-                toggle_fn,
-                { desc = desc }
-            )
-        end
-    end,
 }
