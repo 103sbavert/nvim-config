9c1fe4e2a45aec0ec7a8a89a3c9bede4c39cda5b
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-07 06:36:07 +0530

    style(linting): simplify mason installation mechanism for linters

--- lua/config/plugins/languages/linters.lua
+++ lua/config/plugins/languages/linters.lua
@@ -1,29 +1,32 @@
+local linters_by_ft = {
+    markdown = { "markdownlint-cli2" },
+    javascript = { "eslint_d" },
+    typescript = { "eslint_d" },
+    javascriptreact = { "eslint_d" },
+    typescriptreact = { "eslint_d" },
+}
+
 --- @type LazySpec
 return {
     "mfussenegger/nvim-lint",
     dependencies = { "config.mason" },
     event = { "VeryLazy" },
-    config = function()
-        local lint = require("lint")
-
-        lint.linters_by_ft = {
-            markdown = { "markdownlint-cli2" },
-            javascript = { "eslint_d" },
-            typescript = { "eslint_d" },
-            javascriptreact = { "eslint_d" },
-            typescriptreact = { "eslint_d" },
-        }
-
-        -- Force markdownlint-cli2 to accept input via stdin instead of reading disk files
-        lint.linters["markdownlint-cli2"].args = { "-" }
-        lint.linters["markdownlint-cli2"].stdin = true
-
+    init = function()
         local linters = {}
-        for _, ft_linters in pairs(lint.linters_by_ft or {}) do
+        for _, ft_linters in pairs(linters_by_ft or {}) do
             vim.list_extend(linters, ft_linters)
         end
 
         require("config.mason").InstallTools(linters)
+    end,
+    config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = linters_by_ft
+
+        -- Force markdownlint-cli2 to accept input via stdin instead of reading disk files
+        lint.linters["markdownlint-cli2"].args = { "-" }
+        lint.linters["markdownlint-cli2"].stdin = true
 
         local lint_augroup =
             vim.api.nvim_create_augroup("lint", { clear = true })
