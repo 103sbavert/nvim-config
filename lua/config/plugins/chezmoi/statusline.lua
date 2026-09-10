acf69503c065e0b0a6523e116ef7116c6d4cb678
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-10 06:44:30 +0530

    perf: defer chezmoi statusline until first source file

--- lua/config/plugins/chezmoi/statusline.lua
+++ lua/config/plugins/chezmoi/statusline.lua
@@ -1,23 +1,44 @@
 local CZM_STATUSLINE_HI = "%#MiniStatuslineChezmoi# [chezmoi] %*"
-local shared = require("config.plugins.chezmoi.utils")
 
-local vulgaris = require("bamboo.palette").vulgaris
-local bg = vulgaris.purple
-local fg = vulgaris.contrast
-vim.api.nvim_set_hl(
-    0,
-    "MiniStatuslineChezmoi",
-    { fg = fg, bg = bg, bold = true }
-)
-
-local UT = require("config.utils")
-local statusline = require("mini.statusline")
-
--- Async cache: filepath -> boolean. Populated via autocmds so section_fileinfo stays sync.
+local highlight_initialized = false
+local orig_section_fileinfo
 local src_file_cache = {}
+
+local function initialize_statusline()
+    if highlight_initialized then
+        return
+    end
+    highlight_initialized = true
+
+    local vulgaris = require("bamboo.palette").vulgaris
+    vim.api.nvim_set_hl(
+        0,
+        "MiniStatuslineChezmoi",
+        { fg = vulgaris.contrast, bg = vulgaris.purple, bold = true }
+    )
+
+    local statusline = require("mini.statusline")
+    orig_section_fileinfo = statusline.section_fileinfo
+
+    --- @diagnostic disable-next-line: duplicate-set-field
+    statusline.section_fileinfo = function(args)
+        local UT = require("config.utils")
+        local fileinfo = orig_section_fileinfo(args)
+        local src_file = UT.get_current_file()
+
+        if src_file and src_file ~= "" and src_file_cache[src_file] then
+            return CZM_STATUSLINE_HI .. " " .. fileinfo
+        end
+
+        return fileinfo
+    end
+end
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
     callback = function(args)
+        local UT = require("config.utils")
+        local shared = require("config.plugins.chezmoi.utils")
+
         local buf_file = UT.get_current_file(args)
         if not buf_file then
             return
@@ -29,23 +50,13 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
             end
             shared.is_src_file_async(buf_file, function(is_src)
                 src_file_cache[buf_file] = is_src
-                vim.schedule(function() vim.cmd("redrawstatus") end)
+                if is_src then
+                    vim.schedule(function()
+                        initialize_statusline()
+                        vim.cmd("redrawstatus")
+                    end)
+                end
             end)
         end)
     end,
 })
-
--- Cache the original function to keep standard file info intact
-local orig_section_fileinfo = statusline.section_fileinfo
-
---- @diagnostic disable-next-line: duplicate-set-field
-statusline.section_fileinfo = function(args)
-    local fileinfo = orig_section_fileinfo(args)
-    local src_file = UT.get_current_file()
-
-    if src_file and src_file ~= "" and src_file_cache[src_file] then
-        return CZM_STATUSLINE_HI .. " " .. fileinfo
-    end
-
-    return fileinfo
-end
