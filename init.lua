862a71931c3ccd455edda1668e45c542eee80319
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-12 08:12:05 +0530

    feat: enable window move keymaps in all modes

--- init.lua
+++ init.lua
@@ -254,10 +254,30 @@ do
     )
 
     -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
-    -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
-    -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
-    -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
-    -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+    vim.keymap.set(
+        { "n", "i", "x", "t" },
+        "<C-S-h>",
+        "<C-w>H",
+        { desc = "Move window to the left" }
+    )
+    vim.keymap.set(
+        { "n", "i", "x", "t" },
+        "<C-S-l>",
+        "<C-w>L",
+        { desc = "Move window to the right" }
+    )
+    vim.keymap.set(
+        { "n", "i", "x", "t" },
+        "<C-S-j>",
+        "<C-w>J",
+        { desc = "Move window to the lower" }
+    )
+    vim.keymap.set(
+        { "n", "i", "x", "t" },
+        "<C-S-k>",
+        "<C-w>K",
+        { desc = "Move window to the upper" }
+    )
 
     -- [[ Basic Autocommands ]]
     --  See `:help lua-guide-autocommands`
