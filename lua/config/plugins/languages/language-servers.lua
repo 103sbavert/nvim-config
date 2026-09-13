16ef98152cf4c0bb41783b88a3b99e9311cc33ba
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-09-13 17:04:01 +0530

    fix(roslyn): lazy-load roslyn plugin by ft, avoid duplicate roslyn_ls

--- lua/config/plugins/languages/language-servers.lua
+++ lua/config/plugins/languages/language-servers.lua
@@ -1,86 +1,103 @@
 --- @type LazySpec
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "config.mason",
-        "seblyng/roslyn.nvim",
-        "config.utils",
-        "j-hui/fidget.nvim",
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "config.mason",
+            "config.utils",
+            "j-hui/fidget.nvim",
+        },
+        config = function()
+            local lang_utils =
+                require("config.plugins.languages.internal.utils")
+
+            -- NOTE: roslyn.nvim enables "roslyn" server on its own (see:
+            -- plugin/roslyn.lua), so the LSP can stay out of this map.
+
+            -- Enable the following language servers
+            --- @type table<string, vim.lsp.Config>
+            local server_config_map = {
+                vtsls = {},
+                eslint = {},
+                bashls = {},
+                gopls = {
+                    settings = {
+                        gopls = {
+                            gofumpt = true,
+                            semanticTokens = true,
+                            hints = {
+                                assignVariableTypes = true,
+                                compositeLiteralFields = true,
+                                compositeLiteralTypes = true,
+                                constantValues = true,
+                                functionTypeParameters = true,
+                                parameterNames = true,
+                                rangeVariableTypes = true,
+                            },
+                            analyses = {
+                                unusedparams = true,
+                                shadow = true,
+                            },
+                            staticcheck = true,
+                        },
+                    },
+                },
+                gitlab_ci_ls = {},
+                pyright = {},
+                stylua = {},
+                lua_ls = {
+                    on_attach = function(client)
+                        client.server_capabilities.documentFormattingProvider =
+                            false
+                        client.server_capabilities.documentRangeFormattingProvider =
+                            false
+                    end,
+                    on_init = function(client)
+                        if
+                            not lang_utils.is_nvim_config(
+                                client.workspace_folders
+                            )
+                            and lang_utils.has_lua_config(
+                                client.workspace_folders
+                            )
+                        then
+                            return
+                        end
+
+                        local base_ls_opts = client.config.settings.Lua
+                        --- @cast base_ls_opts table
+                        client.config.settings.Lua =
+                            lang_utils.get_nvim_lua_opts(base_ls_opts)
+                    end,
+                    settings = {
+                        Lua = {
+                            format = { enable = false },
+                            diagnostics = { disable = { "missing-fields" } },
+                        },
+                    },
+                },
+            }
+
+            local server_names = vim.tbl_keys(server_config_map or {})
+
+            -- NOTE: only install as the configuration is handled externally, like config below
+            local managed = { "roslyn_ls" }
+            vim.list_extend(server_names, managed)
+
+            require("config.mason").InstallTools(server_names)
+            require("config.plugins.languages.internal.autocmds")
+
+            for name, server_conf in pairs(server_config_map) do
+                vim.lsp.config(name, server_conf)
+                vim.lsp.enable(name)
+            end
+        end,
     },
-    config = function()
-        local lang_utils = require("config.plugins.languages.internal.utils")
-
-        -- Enable the following language servers
-        --- @type table<string, vim.lsp.Config>
-        local server_config_map = {
-            roslyn_ls = {
-                before_init = function() require("roslyn").setup() end,
-            },
-            vtsls = {},
-            eslint = {},
-            bashls = {},
-            gopls = {
-                settings = {
-                    gopls = {
-                        gofumpt = true,
-                        semanticTokens = true,
-                        hints = {
-                            assignVariableTypes = true,
-                            compositeLiteralFields = true,
-                            compositeLiteralTypes = true,
-                            constantValues = true,
-                            functionTypeParameters = true,
-                            parameterNames = true,
-                            rangeVariableTypes = true,
-                        },
-                        analyses = {
-                            unusedparams = true,
-                            shadow = true,
-                        },
-                        staticcheck = true,
-                    },
-                },
-            },
-            gitlab_ci_ls = {},
-            pyright = {},
-            stylua = {},
-            lua_ls = {
-                on_attach = function(client)
-                    client.server_capabilities.documentFormattingProvider =
-                        false
-                    client.server_capabilities.documentRangeFormattingProvider =
-                        false
-                end,
-                on_init = function(client)
-                    if
-                        not lang_utils.is_nvim_config(client.workspace_folders)
-                        and lang_utils.has_lua_config(client.workspace_folders)
-                    then
-                        return
-                    end
-
-                    local base_ls_opts = client.config.settings.Lua
-                    --- @cast base_ls_opts table
-                    client.config.settings.Lua =
-                        lang_utils.get_nvim_lua_opts(base_ls_opts)
-                end,
-                settings = {
-                    Lua = {
-                        format = { enable = false },
-                        diagnostics = { disable = { "missing-fields" } },
-                    },
-                },
-            },
-        }
-
-        local server_names = vim.tbl_keys(server_config_map or {})
-
-        require("config.mason").InstallTools(server_names)
-        require("config.plugins.languages.internal.autocmds")
-
-        for name, server_conf in pairs(server_config_map) do
-            vim.lsp.config(name, server_conf)
-            vim.lsp.enable(name)
-        end
-    end,
+    {
+        "seblyng/roslyn.nvim",
+        ft = { "cs", "razor" },
+        ---@module "roslyn.config"
+        ---@type RoslynNvimConfig
+        opts = {},
+    },
 }
